08102068421369c5c542b13697034543bf5d07b5
kim dongmin <ehdals2881@naver.com>
2022-05-12 16:34:01 +0900

    팀원생성기능 수정

--- src/frontend/src/store/index.js
+++ src/frontend/src/store/index.js
@@ -2,87 +2,86 @@ import { createStore } from 'vuex';
 import createPersistedState from 'vuex-persistedstate';
 
 export default createStore({
-    state: {
-        loginState:null,
-        stateCode:null,
-        teamName: '',
-        mcode: '',
-        teamList: [],
-        teamMembers: [],
-        teamMemberList: [],
-        insertName: '',
-        teamCode: '',
-        diff: 0,
-        planCode: '',
-        loginedTeamCode: '',
-        storage:{},
-        payStorage:{},
-        CampingIdOfRooms: '',
-    },
-    getters: {
-        getLoginState(state){
-            return state.loginState
-        },
-        getCampingIdState(state){
-            return state.CampingIdOfRooms
-        }
-    },
-    mutations: {
-        setLoginState(state, loginState, stateCode, mcode){
-            state.loginState = loginState
-            state.stateCode = stateCode
-            state.mcode = mcode
-        },
-        setLogoutState(state){
-            state.loginState = null
-            state.stateCode = null
-            state.mcode = null
-        },
-        updateTeamList(state, teamMember) {
-            state.teamList.push(teamMember);
-        },
-        updateMyTeam(state, mcode) {
-            state.mcode = mcode;
-        },
-        updateTeamName(state, teamName) {
-            state.teamName = teamName;
-        },
-        updateTeamMemberList(state, mname) {
-            state.teamMemberList.push(mname);
-        },
-        updateTeamCode(state, teamCode) {
-            state.teamCode = teamCode;
-        },
-        updateDiff(state, diff) {
-            state.diff = diff;
-        },
-        updatePlanCode(state, planCode) {
-            state.planCode = planCode;
-        },
-        updateLoginedTeamCode(state, loginedTeamCode) {
-            state.loginedTeamCode = loginedTeamCode;
-        },
-        storageCheck(state, storage){
-            state.storage = storage;
-            console.log(state.storage)
-        },
-        storageClear(state){
-            state.storage = {};
-            console.log('지워졌나')
-            console.log(state.storage)
-        },
-        putCartStorage(state, form){
-            state.payStorage = form;
-        },
-        cartStorageClear(state){
-            state.payStorage = {};
-        }
-        setCampingIdState(state,CampingIdOfRooms) {
-            state.CampingIdOfRooms = CampingIdOfRooms;
-        }
-    },
-    actions: {
-    },
+	state: {
+		loginState: null,
+		stateCode: null,
+		teamName: '',
+		mcode: '',
+		teamList: [],
+		teamMembers: [],
+		teamMemberList: [],
+		insertName: '',
+		teamCode: '',
+		diff: 0,
+		planCode: '',
+		loginedTeamCode: '',
+		storage: {},
+		payStorage: {},
+		CampingIdOfRooms: '',
+	},
+	getters: {
+		getLoginState(state) {
+			return state.loginState;
+		},
+		getCampingIdState(state) {
+			return state.CampingIdOfRooms;
+		},
+	},
+	mutations: {
+		setLoginState(state, loginState, stateCode, mcode) {
+			state.loginState = loginState;
+			state.stateCode = stateCode;
+			state.mcode = mcode;
+		},
+		setLogoutState(state) {
+			state.loginState = null;
+			state.stateCode = null;
+			state.mcode = null;
+		},
+		updateTeamList(state, teamMember) {
+			state.teamList.push(teamMember);
+		},
+		updateMyTeam(state, mcode) {
+			state.mcode = mcode;
+		},
+		updateTeamName(state, teamName) {
+			state.teamName = teamName;
+		},
+		updateTeamMemberList(state, mname) {
+			state.teamMemberList.push(mname);
+		},
+		updateTeamCode(state, teamCode) {
+			state.teamCode = teamCode;
+		},
+		updateDiff(state, diff) {
+			state.diff = diff;
+		},
+		updatePlanCode(state, planCode) {
+			state.planCode = planCode;
+		},
+		updateLoginedTeamCode(state, loginedTeamCode) {
+			state.loginedTeamCode = loginedTeamCode;
+		},
+		storageCheck(state, storage) {
+			state.storage = storage;
+			console.log(state.storage);
+		},
+		storageClear(state) {
+			state.storage = {};
+			console.log('지워졌나');
+			console.log(state.storage);
+		},
+		putCartStorage(state, form) {
+			state.payStorage = form;
+		},
+		cartStorageClear(state) {
+			state.payStorage = {};
+		},
+		setCampingIdState(state, CampingIdOfRooms) {
+			state.CampingIdOfRooms = CampingIdOfRooms;
+		},
+	},
+	actions: {},
 
 	plugins: [createPersistedState()],
 });
