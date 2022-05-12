545a9ecface6f9f903ae40ef9dd68862ee84bd2b
KBS <84058090+sabow1@users.noreply.github.com>
2022-05-12 16:44:51 +0900

    Update App.vue

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -32,7 +32,6 @@ html,body{padding:0; margin:0;}
   //text-align: center;
   color: #2c3e50;
 }
-h1{color:#43b984;}
 table{width:100%; border-collapse:collapse;}
 .wrap{width:100%;}
 .container{width:800px; margin:0 auto;}
