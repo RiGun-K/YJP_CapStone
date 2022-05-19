e31ea7bc41bd3378225b14034912d956ff895553
KBS <84058090+sabow1@users.noreply.github.com>
2022-05-19 10:45:04 +0900

    ver3.64 보관소 디자인

--- src/frontend/src/components/storageService/admin/manager/GetStorageManagerPage.vue
+++ src/frontend/src/components/storageService/admin/manager/GetStorageManagerPage.vue
@@ -1,55 +1,40 @@
 <template>
-  <h3 style="margin-top: 5%; margin-left: 5%;">보관소 매니저 관리</h3>
-<!--  <div class="manager-get">-->
-<!--    <div v-for="(manager, index) in storageManagerList" :key="index" style="margin-top: 3%">-->
-<!--      <div class="card" style="width: 25%; font-weight: bolder; margin-left: 7%">-->
-<!--        <div class="card-body">-->
-<!--          보관소: {{ manager.storageCode.storageName }}-->
-<!--        </div>-->
-<!--      </div>-->
-<!--      <div class="card" style="display: flex; width: 25%; margin-left: 7%; margin-bottom: 3%">-->
-<!--        <div class="card-body">-->
-<!--          매니저이름: {{ manager.mcode.mnick }}-->
-<!--        </div>-->
-<!--        <div class="card-body">-->
-<!--          매니저아이디: {{ manager.mcode.mid }}-->
-<!--        </div>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--  </div>-->
-  <!-- 보관소별 관리자 리스트 -->
-
-  <table>
-    <thead>
-    <tr>
-      <th>
-        <select v-model="storagePick">
-          <option value="전체">전체</option>
-          <option v-for="pick in pickList" :value="pick.code">{{pick.name}}</option>
-        </select>
-      </th>
-      <th>보관소명</th>
-      <th>관리자 이름</th>
-      <th>수정/변경</th>
-    </tr>
-    </thead>
-    <tbody v-if="storagePick == '전체'" v-for="(manager,index) in storageManagerList" :key="index">
+  <div class="allBody">
+    <div class="titleBody">
+      <h3 style="margin-top: 5%; margin-left: 5%;">보관소 매니저 관리</h3>
+    </div>
+    <table class="tableBody">
+      <thead>
+      <tr>
+        <th>
+          <select v-model="storagePick">
+            <option value="전체">전체</option>
+            <option v-for="pick in pickList" :value="pick.code">{{pick.name}}</option>
+          </select>
+        </th>
+        <th>보관소명</th>
+        <th>관리자 이름</th>
+        <th>수정/변경</th>
+      </tr>
+      </thead>
+      <tbody v-if="storagePick == '전체'" v-for="(manager,index) in storageManagerList" :key="index">
       <tr>
         <td>{{index+1}}.</td>
         <td>{{manager.storageCode.storageName}}</td>
         <td>{{ manager.mcode.mnick }}</td>
         <td><button @click="edit(torageManagerCode)">수정</button><button @click="remove(torageManagerCode)">삭제</button></td>
       </tr>
-    </tbody>
-    <tbody v-else v-for="(manager,index) in managerList">
+      </tbody>
+      <tbody v-else v-for="(manager,index) in managerList">
       <tr>
         <td>{{index+1}}.</td>
         <td>{{manager.storageCode.storageName}}</td>
         <td>{{ manager.mcode.mnick }}</td>
         <td><button>수정</button><button>삭제</button></td>
       </tr>
-    </tbody>
-  </table>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
@@ -121,4 +106,12 @@ export default {
 </script>
 
 <style scoped>
+.allBody{
+  margin: 1% 1% 1% 1%;
+  text-align: center;
+}
+.titleBody{
+  margin-bottom: 1%;
+  text-align: left;
+}
 </style>
