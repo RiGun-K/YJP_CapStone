e59d26cf99eba6b754331539084de2eadacdb9de
kim dongmin <ehdals2881@naver.com>
2022-05-26 19:50:01 +0900

    삭제 수정

--- src/frontend/src/components/plan/selectCopy.vue
+++ src/frontend/src/components/plan/selectCopy.vue
@@ -1,9 +1,24 @@
 <template>
-	<h2>다른 유저들의 계획을 참조할 수 있습니다.</h2>
-	<button @click="reference">참조하기</button>
-
-	<h2>자신만의 플랜을 만들 수 있습니다.</h2>
-	<button @click="individual">참조하지 않음</button>
+	<div class="background">
+		<div class="MenuPosition">
+			<div class="SelectMenu">
+				<button class="SelectBtn" @click="reference">
+					<img src="@\assets\diversity.png" alt="" />
+				</button>
+				<div class="MenuInfoBlock">
+					<p>다른 유저들의 계획을 참조할 수 있습니다.</p>
+				</div>
+			</div>
+			<div class="SelectMenu">
+				<button class="SelectBtn" @click="individual">
+					<img src="@\assets\idea.png" alt="" />
+				</button>
+				<div class="MenuInfoBlock">
+					<p>자신만의 플랜을 만들 수 있습니다.</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -23,4 +38,60 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.background {
+	width: 100vw;
+	height: 100vh;
+	background-color: #84bdff;
+}
+
+.MenuPosition {
+	position: absolute;
+	width: 100%;
+	left: 50%;
+	top: calc(50% + 150px);
+	transform: translate(-50%, -50%);
+	text-align: center;
+}
+
+.SelectMenu {
+	position: relative;
+	display: inline-block;
+	width: 320px;
+	margin: 30px;
+	background: white;
+	padding: 15px;
+	border-radius: 10px;
+}
+
+.SelectBtn {
+	color: yellow;
+	background-color: rgb(160, 233, 193);
+	border: none;
+	width: 100%;
+	padding: 20px;
+	border-radius: 20px;
+}
+
+.SelectBtn img {
+	width: 100%;
+}
+
+.MenuInfoBlock {
+	width: 100%;
+	margin-top: 20px;
+	height: 40px;
+	/* background: #525252; */
+	border-radius: 20px;
+	border: none;
+	text-align: center;
+}
+
+.MenuInfoBlock p {
+	position: relative;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	white-space: nowrap;
+}
+</style>
