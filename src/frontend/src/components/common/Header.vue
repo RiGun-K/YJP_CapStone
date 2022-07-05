c7c7b693ed4d53f5dd4474239d2ac80eadb01445
Yura Park <pyr101116@gmail.com>
2022-07-05 16:49:34 +0900

    0705

--- src/frontend/src/components/common/Header.vue
+++ src/frontend/src/components/common/Header.vue
@@ -50,11 +50,15 @@
     <symbol id="manager" viewBox="0 0 16 16">
       <path d="M7.752.066a.5.5 0 0 1 .496 0l3.75 2.143a.5.5 0 0 1 .252.434v3.995l3.498 2A.5.5 0 0 1 16 9.07v4.286a.5.5 0 0 1-.252.434l-3.75 2.143a.5.5 0 0 1-.496 0l-3.502-2-3.502 2.001a.5.5 0 0 1-.496 0l-3.75-2.143A.5.5 0 0 1 0 13.357V9.071a.5.5 0 0 1 .252-.434L3.75 6.638V2.643a.5.5 0 0 1 .252-.434L7.752.066ZM4.25 7.504 1.508 9.071l2.742 1.567 2.742-1.567L4.25 7.504ZM7.5 9.933l-2.75 1.571v3.134l2.75-1.571V9.933Zm1 3.134 2.75 1.571v-3.134L8.5 9.933v3.134Zm.508-3.996 2.742 1.567 2.742-1.567-2.742-1.567-2.742 1.567Zm2.242-2.433V3.504L8.5 5.076V8.21l2.75-1.572ZM7.5 8.21V5.076L4.75 3.504v3.134L7.5 8.21ZM5.258 2.643 8 4.21l2.742-1.567L8 1.076 5.258 2.643ZM15 9.933l-2.75 1.571v3.134L15 13.067V9.933ZM3.75 14.638v-3.134L1 9.933v3.134l2.75 1.571Z"/>
     </symbol>
-
     <symbol id="master" viewBox="0 0 16 16">
       <path d="M4 16s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H4Zm4-5.95a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5Z"/>
       <path d="M2 1a2 2 0 0 0-2 2v9.5A1.5 1.5 0 0 0 1.5 14h.653a5.373 5.373 0 0 1 1.066-2H1V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v9h-2.219c.554.654.89 1.373 1.066 2h.653a1.5 1.5 0 0 0 1.5-1.5V3a2 2 0 0 0-2-2H2Z"/>
     </symbol>
+    <symbol id="logout" viewBox="0 0 16 16">
+      <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+      <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+    </symbol>
+
 
 
   </svg>
@@ -127,7 +131,7 @@
           </div>
         </div>
       </div>
-      <div class="px-3 py-2 border-bottom mb-3" style="background-color: #f6f6f6; width: 68%; margin-left: 16%">
+      <div class="px-3 py-2 border-bottom mb-3">
         <div class="container d-flex flex-wrap justify-content-center">
           <form class="col-12 col-lg-auto mb-2 mb-lg-0 me-lg-auto" role="search">
             <input type="search" class="form-control" placeholder="Search..." aria-label="Search">
@@ -138,13 +142,31 @@
             <button type="button" class="btn btn-primary" @click="login" v-if="!uiLogin">Login</button>
 
             <div style="display: flex">
-              <a href="/memberUpdate" class="nav-link text-black"  v-if="uiLogin">
-                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
-              </a>
+              <div class="flex-shrink-0 dropdown" v-if="uiLogin!='' && uiLogin!=5" style="padding-top: 9px">
+                <a href="/" class="d-block link-dark text-decoration-none" data-bs-toggle="dropdown">
+                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>
+                </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser2">
+                  <li><a class="dropdown-item" href="/memberUpdate">정보 수정</a></li>
+                  <li><a class="dropdown-item" href="/changePass">비밀번호 변경</a></li>
+                  <li><a class="dropdown-item" href="/myPreference">선호도 관리</a></li>
+                  <li><a class="dropdown-item" href="/myPageWritter">게시글 목록</a></li>
+                  <li><a class="dropdown-item" href="/myPageEquip">장비 목록</a></li>
+                  <li><a class="dropdown-item" href="/myBox">보관함 관리</a></li>
+                  <li><a class="dropdown-item" href="/ordersList">주문 목록</a></li>
+                  <li v-if="uiLogin==3"><a class="dropdown-item" href="/company">판매자 신청</a></li>
+
+                  <li><hr class="dropdown-divider"></li>
+                  <li @click="logout" style="display: flex"><a class="dropdown-item" >Sign out</a></li>
+                </ul>
+              </div>
+<!--              <a href="/memberUpdate" class="nav-link text-black"  v-if="uiLogin">-->
+<!--                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"/></svg>-->
+<!--              </a>-->
               <a href="/cart" class="nav-link text-black"  v-if="uiLogin">
                 <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#cart" /></svg>
               </a>
-              <button type="button" class="btn btn-light text-dark me-2" @click="logout" v-if="uiLogin">Logout</button>
+              <button type="button" class="btn btn-light text-dark me-2" @click="logout" v-if="uiLogin!='' && uiLogin==5">Sign out</button>
             </div>
           </div>
         </div>
@@ -220,6 +242,19 @@ export default {
   font-size: 85%;
 }
 
+@media screen and (min-width: 1400px) {
+  .px-3 py-2 border-bottom mb-3{
+    max-width: 71%;
+    margin-left: 15%;
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .px-3 py-2 border-bottom mb-3{
+    max-width: 71%;
+    margin-left: 15%;
+  }
+}
 
 /*header{*/
 /*  width:100%;*/
